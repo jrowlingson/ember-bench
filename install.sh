fee3e198464385cc76d7d2ab4ecd8464159a4585
Jack Rowlingson <jack.rowlingson.dev@gmailcom>
2020-08-24 19:41:51 -0400

    fix install (again)

--- install.sh
+++ install.sh
@@ -3,9 +3,8 @@ repo_root=https://raw.githubusercontent.com/jrowlingson/ember-brew/master/
 
 # install ember-bench command
 sudo curl -o /usr/local/bin/ember-bench ${repo_root}ember-bench
-sudo chmod 755 `whoami` /usr/local/bin/ember-bench
 
 # install ember-bench lib
 sudo mkdir -p /usr/local/lib/ember-bench/
-sudo curl -o /usr/local/lib/ember-bench/bench.zsh ${repo_root}ember-brew/lib/bench.zsh
-sudo curl -o /usr/local/lib/ember-bench/print_env.zsh ${repo_root}ember-brew/lib/print_env.zsh
+sudo curl -o /usr/local/lib/ember-bench/bench.zsh ${repo_root}lib/bench.zsh
+sudo curl -o /usr/local/lib/ember-bench/print_env.zsh ${repo_root}lib/print_env.zsh
